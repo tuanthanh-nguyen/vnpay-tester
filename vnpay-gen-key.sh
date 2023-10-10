fc1db8214f6bb81c73049184f484186ea161af3d
Clueless Clown <thanhnguyen@emddi.com>
2023-10-10 17:32:45 +0700

    fix: auto generate cert without prompt

--- vnpay-gen-key.sh
+++ vnpay-gen-key.sh
@@ -11,4 +11,12 @@ openssl genrsa -out $HOME/vnpay_prod/private-k.pem 2048
 openssl rsa -in $HOME/vnpay_prod/private-k.pem -out $HOME/vnpay_prod/public-k.pem --outform PEM -pubout
 
 # generate cert
-printf 'VN\nHN\nHN\nEMDDI\nEMDDI\nEMDDI\nemddijsc@emddi.com' | openssl req -new -x509 -nodes -sha256 -days 3000 -key $HOME/vnpay_prod/private-k.pem -out $HOME/vnpay_prod/vnpay_public_cert.cer
+openssl req \
+-new \
+-x509 \
+-nodes \
+-sha256 \
+-days 3000 \
+-subj "/C=VN/ST=HN/L=HN/O=EMDDI/CN=EMDDI" \
+-key $HOME/vnpay_prod/private-k.pem \
+-out $HOME/vnpay_prod/vnpay_public_cert \
